434c908ba9c974ceef50ecafe49a597086290264
YuriiZ <yuriiz@zzz.ggwp>
2022-06-22 22:21:29 +0300

    1 lesson done

--- 01/111.py
+++ 01/111.py
@@ -6,9 +6,72 @@
 
 #!/usr/bin/env python
 
-print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
-print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
-print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
-print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
-print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
-# print()
+# print('1'+'2')
+# print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!')
+# print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
+# print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
+# print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
+# print('', 1, 3, 5, 7, 11, '', sep='_', end='!!!\n')
+# print?
+
+
+
+# 
+
+# def repeat(s, exclaim):
+#     """
+#     Returns the string 's' repeated 3 times.
+#     If exclaim is true, add exclamation marks.
+#     """
+
+#     result = s + s + s # can also use "s * 3" which is faster (Why?)
+#     if exclaim:
+#         result = result + '!!!'
+#     return result
+
+# print(len(repeat('2',1)))
+print('!!!!!!!!!\n'*5)
+print(
+      # '\n'
+       '#'*9+'\n'
+      + '#\t\t#\n'
+      # + '#'+'\t'*7+'#\n'
+      + '#\t\t#\n'
+      + '#\t\t#\n'
+      + '#'*9    +'\n')
+
+# print('#'+'\t'*7+'#\n#'+'\t'*7+'#\n'   
+#       +  '#'*9+'\n' 
+#       +'#'+'\t'*7+'#\n#'+'\t'*7+'#\n'  )
+
+print('#\t\t#\n'
+      + '#\t\t#\n'
+      + '#'*9+'\n'
+      + '#\t\t#\n'
+      + '#\t\t#\n' )
+
+print('#\t\t#\n'
+      + '##     ##\n'
+      + '# #   # #\n'
+      + '#  # #  #\n'
+      + '#\t#\t#\n'
+      )
+
+print('!!!!!!!!!\n'*5)
+ 
+
+print('hello'[0])
+ 
+s='0123456789'
+
+
+
+
+
+
+
+#       #
+##     ##
+# #   # #
+#  # #  #
+#   #   #')
